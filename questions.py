f66c40035d59fd56d8a5c77ec7375209a80fcfcd
Vaishali <vv1705260@gmail.com>
2026-05-11 17:56:46 +0530

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,4 +1,4 @@
-quiz_questions = [
+Sample Demo quiz_questions = [
 
     {
         "question": "What is the extension of Python files?",
@@ -82,6 +82,360 @@ quiz_questions = [
         ],
         "answer": "C",
         "correct_text": "def"
+    }
+
+]
+here We Start Quiz now
+quiz_questions = [
+
+    {
+        "question": "What is the extension of Python files?",
+        "options": ["A. .html", "B. .java", "C. .py", "D. .cpp"],
+        "answer": "C",
+        "correct_text": ".py"
+    },
+
+    {
+        "question": "Who developed Python?",
+        "options": ["A. Dennis Ritchie", "B. Guido van Rossum", "C. James Gosling", "D. Elon Musk"],
+        "answer": "B",
+        "correct_text": "Guido van Rossum"
+    },
+
+    {
+        "question": "Which keyword is used for loops in Python?",
+        "options": ["A. repeat", "B. loop", "C. iterate", "D. for"],
+        "answer": "D",
+        "correct_text": "for"
+    },
+
+    {
+        "question": "Which symbol is used for comments in Python?",
+        "options": ["A. //", "B. <!-- -->", "C. #", "D. **"],
+        "answer": "C",
+        "correct_text": "#"
+    },
+
+    {
+        "question": "Which function is used to take input in Python?",
+        "options": ["A. print()", "B. scanf()", "C. cin", "D. input()"],
+        "answer": "D",
+        "correct_text": "input()"
+    },
+
+    {
+        "question": "Which data type stores decimal values?",
+        "options": ["A. int", "B. float", "C. bool", "D. str"],
+        "answer": "B",
+        "correct_text": "float"
+    },
+
+    {
+        "question": "Which keyword defines a function?",
+        "options": ["A. define", "B. function", "C. def", "D. func"],
+        "answer": "C",
+        "correct_text": "def"
+    },
+
+    {
+        "question": "Which operator is used for exponentiation?",
+        "options": ["A. ^", "B. **", "C. //", "D. %"],
+        "answer": "B",
+        "correct_text": "**"
+    },
+
+    {
+        "question": "Which collection type is ordered and changeable?",
+        "options": ["A. Set", "B. Tuple", "C. List", "D. Dictionary"],
+        "answer": "C",
+        "correct_text": "List"
+    },
+
+    {
+        "question": "Which function gives length of a list?",
+        "options": ["A. size()", "B. count()", "C. len()", "D. length()"],
+        "answer": "C",
+        "correct_text": "len()"
+    },
+
+    {
+        "question": "Which keyword is used for conditions?",
+        "options": ["A. switch", "B. case", "C. if", "D. check"],
+        "answer": "C",
+        "correct_text": "if"
+    },
+
+    {
+        "question": "Which loop runs forever if condition always true?",
+        "options": ["A. for", "B. while", "C. nested", "D. do while"],
+        "answer": "B",
+        "correct_text": "while"
+    },
+
+    {
+        "question": "Which function converts string to integer?",
+        "options": ["A. str()", "B. int()", "C. float()", "D. bool()"],
+        "answer": "B",
+        "correct_text": "int()"
+    },
+
+    {
+        "question": "Which keyword exits a loop?",
+        "options": ["A. stop", "B. continue", "C. exit", "D. break"],
+        "answer": "D",
+        "correct_text": "break"
+    },
+
+    {
+        "question": "Which keyword skips current iteration?",
+        "options": ["A. pass", "B. continue", "C. next", "D. skip"],
+        "answer": "B",
+        "correct_text": "continue"
+    },
+
+    {
+        "question": "Which data type is immutable?",
+        "options": ["A. List", "B. Dictionary", "C. Tuple", "D. Set"],
+        "answer": "C",
+        "correct_text": "Tuple"
+    },
+
+    {
+        "question": "Which module is used for random values?",
+        "options": ["A. math", "B. random", "C. os", "D. sys"],
+        "answer": "B",
+        "correct_text": "random"
+    },
+
+    {
+        "question": "Which function prints output?",
+        "options": ["A. display()", "B. print()", "C. echo()", "D. output()"],
+        "answer": "B",
+        "correct_text": "print()"
+    },
+
+    {
+        "question": "Which module works with operating system?",
+        "options": ["A. random", "B. time", "C. os", "D. math"],
+        "answer": "C",
+        "correct_text": "os"
+    },
+
+    {
+        "question": "Which method adds item to list?",
+        "options": ["A. add()", "B. append()", "C. insert()", "D. push()"],
+        "answer": "B",
+        "correct_text": "append()"
+    },
+
+    {
+        "question": "Which function opens files?",
+        "options": ["A. file()", "B. open()", "C. fopen()", "D. read()"],
+        "answer": "B",
+        "correct_text": "open()"
+    },
+
+    {
+        "question": "Which keyword handles exceptions?",
+        "options": ["A. error", "B. except", "C. catch", "D. final"],
+        "answer": "B",
+        "correct_text": "except"
+    },
+
+    {
+        "question": "Which module handles dates?",
+        "options": ["A. datetime", "B. random", "C. os", "D. math"],
+        "answer": "A",
+        "correct_text": "datetime"
+    },
+
+    {
+        "question": "Which function removes spaces?",
+        "options": ["A. trim()", "B. strip()", "C. remove()", "D. delete()"],
+        "answer": "B",
+        "correct_text": "strip()"
+    },
+
+    {
+        "question": "Which symbol is used for list indexing?",
+        "options": ["A. {}", "B. ()", "C. []", "D. <>"],
+        "answer": "C",
+        "correct_text": "[]"
+    },
+
+    {
+        "question": "Which keyword creates class?",
+        "options": ["A. object", "B. class", "C. define", "D. struct"],
+        "answer": "B",
+        "correct_text": "class"
+    },
+
+    {
+        "question": "Which method converts to uppercase?",
+        "options": ["A. upper()", "B. capital()", "C. big()", "D. up()"],
+        "answer": "A",
+        "correct_text": "upper()"
+    },
+
+    {
+        "question": "Which operator checks equality?",
+        "options": ["A. =", "B. !=", "C. ==", "D. >="],
+        "answer": "C",
+        "correct_text": "=="
+    },
+
+    {
+        "question": "Which method removes item from list?",
+        "options": ["A. pop()", "B. clear()", "C. delete()", "D. erase()"],
+        "answer": "A",
+        "correct_text": "pop()"
+    },
+
+    {
+        "question": "Which collection stores key-value pairs?",
+        "options": ["A. List", "B. Set", "C. Tuple", "D. Dictionary"],
+        "answer": "D",
+        "correct_text": "Dictionary"
+    },
+
+    {
+        "question": "Which operator performs floor division?",
+        "options": ["A. /", "B. //", "C. %", "D. **"],
+        "answer": "B",
+        "correct_text": "//"
+    },
+
+    {
+        "question": "Which function sorts a list?",
+        "options": ["A. arrange()", "B. order()", "C. sort()", "D. sortedlist()"],
+        "answer": "C",
+        "correct_text": "sort()"
+    },
+
+    {
+        "question": "Which keyword is used to import modules?",
+        "options": ["A. include", "B. using", "C. import", "D. require"],
+        "answer": "C",
+        "correct_text": "import"
+    },
+
+    {
+        "question": "Which operator means NOT equal?",
+        "options": ["A. ==", "B. !=", "C. >=", "D. <="],
+        "answer": "B",
+        "correct_text": "!="
+    },
+
+    {
+        "question": "Which data type stores True/False?",
+        "options": ["A. str", "B. int", "C. bool", "D. float"],
+        "answer": "C",
+        "correct_text": "bool"
+    },
+
+    {
+        "question": "Which loop is best for known iterations?",
+        "options": ["A. while", "B. for", "C. infinite", "D. nested"],
+        "answer": "B",
+        "correct_text": "for"
+    },
+
+    {
+        "question": "Which keyword does nothing?",
+        "options": ["A. continue", "B. skip", "C. pass", "D. stop"],
+        "answer": "C",
+        "correct_text": "pass"
+    },
+
+    {
+        "question": "Which function converts to string?",
+        "options": ["A. str()", "B. string()", "C. txt()", "D. convert()"],
+        "answer": "A",
+        "correct_text": "str()"
+    },
+
+    {
+        "question": "Which method splits strings?",
+        "options": ["A. cut()", "B. divide()", "C. split()", "D. break()"],
+        "answer": "C",
+        "correct_text": "split()"
+    },
+
+    {
+        "question": "Which keyword creates inheritance?",
+        "options": ["A. extends", "B. inherits", "C. class()", "D. Parent class"],
+        "answer": "D",
+        "correct_text": "Parent class"
+    },
+
+    {
+        "question": "Which function gives maximum value?",
+        "options": ["A. max()", "B. high()", "C. top()", "D. greatest()"],
+        "answer": "A",
+        "correct_text": "max()"
+    },
+
+    {
+        "question": "Which function gives minimum value?",
+        "options": ["A. low()", "B. min()", "C. small()", "D. least()"],
+        "answer": "B",
+        "correct_text": "min()"
+    },
+
+    {
+        "question": "Which symbol starts dictionary?",
+        "options": ["A. []", "B. {}", "C. ()", "D. <>"],
+        "answer": "B",
+        "correct_text": "{}"
+    },
+
+    {
+        "question": "Which method joins strings?",
+        "options": ["A. join()", "B. merge()", "C. combine()", "D. attach()"],
+        "answer": "A",
+        "correct_text": "join()"
+    },
+
+    {
+        "question": "Which keyword creates anonymous function?",
+        "options": ["A. lambda", "B. anonymous", "C. func", "D. def"],
+        "answer": "A",
+        "correct_text": "lambda"
+    },
+
+    {
+        "question": "Which operator finds remainder?",
+        "options": ["A. //", "B. **", "C. %", "D. /"],
+        "answer": "C",
+        "correct_text": "%"
+    },
+
+    {
+        "question": "Which function rounds numbers?",
+        "options": ["A. floor()", "B. round()", "C. int()", "D. ceil()"],
+        "answer": "B",
+        "correct_text": "round()"
+    },
+
+    {
+        "question": "Which library is used for GUI?",
+        "options": ["A. tkinter", "B. numpy", "C. pandas", "D. flask"],
+        "answer": "A",
+        "correct_text": "tkinter"
+    },
+
+    {
+        "question": "Which framework is used for web apps?",
+        "options": ["A. pygame", "B. flask", "C. turtle", "D. math"],
+        "answer": "B",
+        "correct_text": "flask"
+    },
+
+    {
+        "question": "Which library is used for data analysis?",
+        "options": ["A. pandas", "B. tkinter", "C. pygame", "D. os"],
+        "answer": "A",
+        "correct_text": "pandas"
     }
 
 ]
