a17bea7dfed80c1b42f0c714a48fdfc4cb824c95
Vaishali <vv1705260@gmail.com>
2026-05-11 17:46:01 +0530

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,58 @@
+quiz_questions = [
+
+    {
+        "question": "What is the extension of Python files?",
+        "options": [
+            "A. .html",
+            "B. .java",
+            "C. .py",
+            "D. .cpp"
+        ],
+        "answer": "C"
+    },
+
+    {
+        "question": "Who developed Python?",
+        "options": [
+            "A. Dennis Ritchie",
+            "B. Guido van Rossum",
+            "C. James Gosling",
+            "D. Elon Musk"
+        ],
+        "answer": "B"
+    },
+
+    {
+        "question": "Which keyword is used for loops in Python?",
+        "options": [
+            "A. repeat",
+            "B. loop",
+            "C. iterate",
+            "D. for"
+        ],
+        "answer": "D"
+    },
+
+    {
+        "question": "Which symbol is used for comments in Python?",
+        "options": [
+            "A. //",
+            "B. <!-- -->",
+            "C. #",
+            "D. **"
+        ],
+        "answer": "C"
+    },
+
+    {
+        "question": "Which function is used to take input?",
+        "options": [
+            "A. print()",
+            "B. scanf()",
+            "C. cin",
+            "D. input()"
+        ],
+        "answer": "D"
+    }
+
+]
